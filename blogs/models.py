b7257627258c0c47adc80c1e83137613e9d0155a
yuji-matsunami <yuji.sohuto@gmail.com>
2021-11-03 01:42:41 +0900

    add models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    blog_text = models.TextField()
+    published = models.DateTimeField()
+    image = models.ImageField(upload_to='media/', blank=True)
